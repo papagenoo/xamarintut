af02a60b7831571acfffccf1042d86f06cf2e922
Max <boytsov.maxim@gmail.com>
2014-05-15 04:37:57 +0400

    *

---
+++ Hello_MultiScreen_iPhone/obj/Xcode/0/HomeScreen.h
@@ -0,0 +1,21 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface HomeScreen : UIViewController {
+	UIButton *_btnHelloUniverse;
+	UIButton *_btnHelloWorld;
+}
+
+@property (nonatomic, retain) IBOutlet UIButton *btnHelloUniverse;
+
+@property (nonatomic, retain) IBOutlet UIButton *btnHelloWorld;
+
+@end
